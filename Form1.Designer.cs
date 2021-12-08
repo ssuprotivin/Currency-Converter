093e9e3d633b08102fb9d90a7202900e32bbb967
Suprotivin Sergei <serzh.suprotivin@gmail.com>
2021-12-08 21:57:12 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,390 @@
+﻿
+namespace Конвертер_валют
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(431, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "-->";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Modern No. 20", 8.249999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(57, 15);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(292, 14);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "Введите, пожалуйста, актуальные курсы валют";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(46, 62);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(61, 21);
+            this.comboBox1.TabIndex = 26;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(705, 238);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(91, 21);
+            this.comboBox2.TabIndex = 27;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(146, 238);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(77, 21);
+            this.comboBox3.TabIndex = 28;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(195, 62);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(61, 21);
+            this.comboBox4.TabIndex = 29;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(145, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "to";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(294, 60);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 32;
+            this.button3.Text = "Ввод";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.comboBox4);
+            this.panel1.Location = new System.Drawing.Point(567, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(376, 149);
+            this.panel1.TabIndex = 33;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(567, 335);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(376, 145);
+            this.panel2.TabIndex = 34;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(38, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 35;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(198, 66);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 35;
+            this.button4.Text = "Ввод";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 8.249999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(118, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 14);
+            this.label3.TabIndex = 35;
+            this.label3.Text = "Добавление валюты";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Location = new System.Drawing.Point(31, 335);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(297, 145);
+            this.panel3.TabIndex = 35;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(57, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 13);
+            this.label5.TabIndex = 36;
+            this.label5.Text = "l5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Modern No. 20", 8.249999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(110, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 14);
+            this.label4.TabIndex = 36;
+            this.label4.Text = "История";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(567, 237);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 36;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(273, 237);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 37;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Modern No. 20", 8.249999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(57, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(158, 14);
+            this.label6.TabIndex = 38;
+            this.label6.Text = "Актуальные курсы валют";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 37);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(15, 13);
+            this.label7.TabIndex = 39;
+            this.label7.Text = "l5";
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel4.Controls.Add(this.label7);
+            this.panel4.Controls.Add(this.label6);
+            this.panel4.Location = new System.Drawing.Point(31, 12);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(297, 149);
+            this.panel4.TabIndex = 40;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(431, 250);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 41;
+            this.button2.Text = "<--";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(253, 167);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 42;
+            this.button5.Text = "Удалить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Modern No. 20", 8.249999F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(28, 172);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(211, 14);
+            this.label8.TabIndex = 43;
+            this.label8.Text = "Удалить актуальные курсы валют";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Конвертер_валют.Properties.Resources.Без_названия;
+            this.pictureBox1.Location = new System.Drawing.Point(383, 335);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 134);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 44;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(968, 532);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Конвертер валют";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Конвертер_валют
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int cbInd1 = -1;
+        int cbInd2 = -1;
+        int cbInd3 = -1;
+        int cbInd4 = -1;
+
+        string cbItem1;
+        string cbItem2;
+        string cbItem3;
+        string cbItem4;
+
+        
+        Dictionary<string, double> CurCur = new Dictionary<string, double>();
+
+        List<string> curr = new List<string>() { "USD","EUR", "RUB" };
+
+        
+
+        public bool DigitsOnly (string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    if (c == '.' || c ==',')
+                        return true;
+                else
+                    return false;
+            }
+            return true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            label5.Text = "";
+            label7.Text = "";
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+
+            for (int i = 0; i < curr.Count; i++)
+            {
+                comboBox1.Items.Add(curr[i]);
+                comboBox2.Items.Add(curr[i]);
+                comboBox3.Items.Add(curr[i]);
+                comboBox4.Items.Add(curr[i]);
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double mult = 0;
+            string b = cbItem3 + cbItem2;
+            string c = cbItem2 + cbItem3;
+                     
+            if (cbInd3 > -1 && cbInd2 > -1)
+            {
+                if (cbItem3 != cbItem2)
+                {
+                    if (!string.IsNullOrWhiteSpace(textBox4.Text))
+                    {
+                        if ( DigitsOnly (textBox4.Text) )
+                        {
+                            if (CurCur.ContainsKey(b))
+                                mult = CurCur[b];
+                            else if (CurCur.ContainsKey(c))
+                                mult = 1 / CurCur[c];
+                            else
+                                MessageBox.Show("Ошибка - для данной пары валют не добавлен актуальный курс");
+
+                            if (mult > 0)
+                            {
+                                double First = Convert.ToDouble(textBox4.Text);
+                                double Second = mult * First;
+                                textBox3.Text = Second.ToString();
+                                label5.Text += First+ " " + cbItem3  +" " + "---->"+ " " + Second + " " + cbItem2 +"\n";
+                            }
+                            
+                        }
+                        else
+                            MessageBox.Show("Ошибка - в текстбокс вводятся только  положительные числа");
+
+                    }
+                    else
+                        MessageBox.Show("Ошибка - для перевода заполните левый текстбокс");
+                }
+                else
+                    MessageBox.Show("Ошибка - для перевода выберите две разные валюты");
+
+            }
+            else
+                MessageBox.Show("Ошибка - выберите два значения валют");
+            
+
+        }
+
+       
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+                        
+            if (!string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                if (!curr.Contains(textBox2.Text))
+                {
+                    curr.Add(textBox2.Text);
+                    comboBox1.Items.Add(textBox2.Text);
+                    comboBox2.Items.Add(textBox2.Text);
+                    comboBox3.Items.Add(textBox2.Text);
+                    comboBox4.Items.Add(textBox2.Text);
+
+                }
+                else
+                    MessageBox.Show("Ошибка - данная валюта уже добавлена");
+                
+            }
+            else
+                MessageBox.Show("Ошибка - введите валюту");
+            
+            textBox2.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string s = cbItem1 + cbItem4;
+
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                if (DigitsOnly(textBox1.Text))
+                {                
+                     if (cbInd1 > -1 && cbInd4 > -1)
+                     {
+                            if (cbInd1 != cbInd4)
+                            {
+                                if (!CurCur.ContainsKey(s))
+                                {
+                                  CurCur.Add(s, Convert.ToDouble(textBox1.Text));
+                                  label7.Text += "1  " + cbItem1 + "  =  " + CurCur[s] + " " + cbItem4 + "\n";
+                                }
+                                else
+                                    MessageBox.Show("Ошибка - курс для данной пары уже присутствует");
+
+                            }
+                            else
+                                MessageBox.Show("Ошибка - чтобы добавить актуальный курс валют, выберите разные валюты в ComboBox");
+
+                     }
+                        else
+                            MessageBox.Show("Ошибка - чтобы добавить актуальный курс валют, выберите оба значения в ComboBox");
+                    
+                }
+                else
+                    MessageBox.Show("Ошибка - в качестве курса может выступать только положительное число");
+            }
+            else
+                MessageBox.Show("Ошибка - введите курс");
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbInd1 = comboBox1.SelectedIndex;
+            cbItem1 = comboBox1.SelectedItem.ToString();
+            
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbInd4 = comboBox4.SelectedIndex;
+            cbItem4 = comboBox4.SelectedItem.ToString();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbInd2 = comboBox2.SelectedIndex;
+            cbItem2 = comboBox2.SelectedItem.ToString();
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbInd3 = comboBox3.SelectedIndex;
+            cbItem3 = comboBox3.SelectedItem.ToString();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+            double mult = 0;
+            string c = cbItem3 + cbItem2;
+            string b = cbItem2 + cbItem3;
+
+            if (cbInd3 > -1 && cbInd2 > -1)
+            {
+                if (cbItem3 != cbItem2)
+                {
+                    if (!string.IsNullOrWhiteSpace(textBox3.Text))
+                    {
+                        if (DigitsOnly(textBox3.Text))
+                        {
+                            if (CurCur.ContainsKey(b))
+                                mult = CurCur[b];
+                            else if (CurCur.ContainsKey(c))
+                                mult = 1 / CurCur[c];
+                            else
+                                MessageBox.Show("Ошибка - для данной пары валют не добавлен актуальный курс");
+
+                            if (mult > 0)
+                            {
+                                double First = Convert.ToDouble(textBox3.Text);
+                                double Second = mult * First;
+                                textBox4.Text = Second.ToString();
+                                label5.Text += First + " " + cbItem2 + " " + "---->" + " " + Second + " " + cbItem3 + "\n";
+                            }
+
+                        }
+                        else
+                            MessageBox.Show("Ошибка - в текстбокс вводятся только  положительные числа");
+
+                    }
+                    else
+                        MessageBox.Show("Ошибка - для перевода заполните  правый текстбокс");
+                }
+                else
+                    MessageBox.Show("Ошибка - для перевода выберите две разные валюты");
+
+            }
+            else
+                MessageBox.Show("Ошибка - выберите два значения валют");
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label7.Text = "";
+            CurCur.Clear();
+            MessageBox.Show ("Актуальные курсы валют удалены");
+        }
+    }
+}
